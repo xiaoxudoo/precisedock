eeee0d57ff27958e9c6a891d43df9621b7c90b58
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2017-09-11 01:28:33 +0800

    build-2017-09-11-01-28-33

--- src/app.vue
+++ src/app.vue
@@ -1,59 +1,73 @@
 <template>
-    <div id="appBody" v-cloak v-bd-statistic>
-        <keep-alive>
-            <router-view></router-view>            
-        </keep-alive>
-    </div>
+	<div id="appBody" v-cloak v-bd-statistic>
+		<keep-alive>
+			<router-view></router-view>
+		</keep-alive>
+	</div>
 </template>
 
 <script>
 module.exports = {
-    data:function () {
-        return {
-        }
-    },
-    components: {
-    },
-    created:function () {
+	data: function() {
+		return {
+		}
+	},
+	components: {
+	},
+	created: function() {
 
-    },
-    directives: {
-        
-    }
+	},
+	directives: {
+
+	}
 };
 </script>
 
 <style lang="scss">
-    @import './common/sass/base.scss';
-    html,body{
-        height: auto;  // set height on the main element in the body
-        background: #fff;
-    }
-    /*这个指令对应的元素当Vue实例化完成前都不显示*/
-    [v-cloak] {
-        display: none;
-    }
-    .centered{
-        text-align: center;
-    }
-    .left{
-        text-align: left;
-    }
-    p.left,
-    p.centered{
-        margin-bottom: 20px;
-        font: 18px "微软雅黑";
-    }
-    p.float{
-        font: 18px "微软雅黑";
-    }
-    .question{
-        margin-top:20px;
-        font-weight: bold!important;
-        font: 18px "微软雅黑";
-    }
-    .answer{
-        font-weight: 700;
-        font: 18px "微软雅黑";
-    }
+@import './common/sass/base.scss';
+html,
+body {
+	height: auto; // set height on the main element in the body
+	background: #fff;
+}
+
+
+/*这个指令对应的元素当Vue实例化完成前都不显示*/
+
+[v-cloak] {
+	display: none;
+}
+
+.centered {
+	text-align: center;
+}
+
+.left {
+	text-align: left;
+}
+
+.router-title {
+	margin-bottom: 45px;
+	p {
+		font-family: 'MicroSoft Yahei';
+		font-size: 23px;
+		color: #000;
+		opacity: 0.9;
+		margin-bottom: 10px;
+		
+	}
+	.bar {
+		display: inline-block;
+		width: 50px;
+		height: 8px;
+		background: #fbe795;
+		font-size: 0;
+		line-height: 0;
+	}
+}
+p.left {
+	text-align: left;
+	font-size: 16px;
+	line-height: 1.5;
+}
 </style>
